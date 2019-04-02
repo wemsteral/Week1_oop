2b3dbd76c3b348ac4aa0a8a4655b148454e80c5a
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-04-02 12:43:10 +0100

    add classes and update rspec

--- oop_1/lib/calculator.rb
+++ oop_1/lib/calculator.rb
@@ -1,11 +1,15 @@
-def add(number_1, number_2)
-  number_1 + number_2
+class Calculator
+  def add(number_1, number_2)
+    number_1 + number_2
+  end
+
+  def subtract(number_1, number_2)
+    number_1 - number_2
+  end
 end
 
-def subtract(number_1, number_2)
-  number_1 - number_2
-end
-
-def print_answer(answer)
-  "The Answer is: #{ answer }"
+class Printer
+  def print_answer(answer)
+    "The Answer is: #{ answer }"
+  end
 end
--- oop_1/spec/calculator_spec.rb
+++ oop_1/spec/calculator_spec.rb
@@ -1,35 +1,41 @@
 require 'calculator'
 
-describe 'A calculator' do
+
+describe Calculator do
   describe '#add' do
     it 'adds two numbers' do
-      expect(add(2, 3)).to eq 5
-      expect(add(6, 4)).to eq 10
-      expect(add(0, 0)).to eq 0
-      expect(add(-1, -1)).to eq -2
-      expect(add(Float::INFINITY, 0)).to eq Float::INFINITY
-      expect(add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+      calc = Calculator.new
+      expect(calc.add(2, 3)).to eq 5
+      expect(calc.add(6, 4)).to eq 10
+      expect(calc.add(0, 0)).to eq 0
+      expect(calc.add(-1, -1)).to eq -2
+      expect(calc.add(Float::INFINITY, 0)).to eq Float::INFINITY
+      expect(calc.add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
     end
   end
 
   describe '#subtract' do
     it 'subtracts two numbers' do
-      expect(subtract(3, 2)).to eq 1
-      expect(subtract(6, 4)).to eq 2
-      expect(subtract(0, 0)).to eq 0
-      expect(subtract(-1, -1)).to eq 0
-      expect(subtract(Float::INFINITY, 0)).to eq Float::INFINITY
-      expect(subtract(Float::INFINITY, Float::INFINITY)).to be_nan
+      calc = Calculator.new
+      expect(calc.subtract(3, 2)).to eq 1
+      expect(calc.subtract(6, 4)).to eq 2
+      expect(calc.subtract(0, 0)).to eq 0
+      expect(calc.subtract(-1, -1)).to eq 0
+      expect(calc.subtract(Float::INFINITY, 0)).to eq Float::INFINITY
+      expect(calc.subtract(Float::INFINITY, Float::INFINITY)).to be_nan
     end
   end
+end
 
+describe Printer do
   describe '#print_answer' do
     it 'pretty prints the answer' do
-      expect(print_answer(4)).to eq "The Answer is: 4"
-      expect(print_answer(6)).to eq "The Answer is: 6"
-      expect(print_answer(-1)).to eq "The Answer is: -1"
-      expect(print_answer(0)).to eq "The Answer is: 0"
-      expect(print_answer(Float::INFINITY)).to eq "The Answer is: Infinity"
+      print = Printer.new
+      expect(print.print_answer(4)).to eq "The Answer is: 4"
+      expect(print.print_answer(6)).to eq "The Answer is: 6"
+      expect(print.print_answer(-1)).to eq "The Answer is: -1"
+      expect(print.print_answer(0)).to eq "The Answer is: 0"
+      expect(print.print_answer(Float::INFINITY)).to eq "The Answer is: Infinity"
     end
   end
 end
